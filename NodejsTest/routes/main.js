e3d284d714db97a6b95a78211c8c98668e198a07
xiang162005 <xiang162005@126.com>
2019-09-07 21:04:35 +0800

    finish the basic version

--- NodejsTest/routes/main.js
+++ NodejsTest/routes/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var rp = require("request-promise");
-var fs = require('fs');
+const rp = require("request-promise");
+const { promisify } = require('util');
+const writesheet = require('./google_sheet.js');
 
 
 // use ablum id get ablum title and ablum userId
@@ -36,7 +37,7 @@ function get_username (userId, title) {
 
     rp(options)
         .then(function (repos) {
-            write_json(title, repos.username);
+            writesheet(title, repos.username);
         })
         .catch(function (err) {
             console.log(err);
@@ -44,46 +45,8 @@ function get_username (userId, title) {
 }
 
 
-// write ablum title and username into json
-function write_json (title, username) {
-    try {
-        var result;
-        // read the json data
-        var data = fs.readFileSync('result.json', 'utf-8');
-        if (data) {
-            result = JSON.parse(data);
-        }
-        else {
-            result = {
-                "ablum_list": []
-            };
-        }
-        // add title and username to data
-        // if writeable is false, the title and username will not add to json
-        var writeable = true;
-        for (let ablum of result.ablum_list) {
-            if (ablum.title === title) {
-                writeable = false;
-                break;
-            }
-        }
-        if (writeable) {
-            result.ablum_list.push({"title": title, "username": username});
-        }
-        // write to json
-        var write_data = JSON.stringify(result);
-        fs.writeFileSync('result.json', write_data);
-        console.log('write into result.json');
-        
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-
-
 // get the ablum_ids
-function get_ablums (ablum_list) {
+async function get_ablums (ablum_list) {
     // ablum_id_list is not Null
     if (ablum_list) {
         for (let ablum_id of ablum_list) {
--- NodejsTest/setup.js
+++ NodejsTest/setup.js
@@ -7,8 +7,12 @@ var get_ablums = require('./routes/main');
 
 // Assume that json is received: {"ablum_list": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
 app.post('/v1/log', function (req, res) {
-    get_ablums(req.body.ablum_list);
-    res.sendStatus(200);
+    success = get_ablums(req.body.ablum_list);
+    if (success) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(400);
+    }
 })
 
 
